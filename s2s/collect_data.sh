00a90e12b3f2c9dc02ba40af4261ae38a399f28a
Reuben Kraft <reuben.kraft@gmail.com>
2020-01-28 18:13:50 -0500

    updates for multiruns

---
+++ s2s/collect_data.sh
@@ -0,0 +1,7 @@
+mkdir collected_data
+rm collected_data/*
+for i in {1..18}
+do
+	echo "directory: scenario_$i"
+	cp scenario_$i/viper3d_th_overpressure.txt collected_data/viper3d_th_overpressure_s$i.txt
+done
