86f310f9ff0a054f459d4d99d9be80c890da1b5a
Reuben Kraft <reuben.kraft@gmail.com>
2020-02-26 16:06:54 +0000

    file organization

--- s2s/collect_data.sh
+++ s2s/collect_data.sh
@@ -1,6 +1,6 @@
 mkdir collected_data
 rm collected_data/*
-for i in {1..18}
+for i in {301..972}
 do
 	echo "directory: scenario_$i"
 	cp scenario_$i/viper3d_th_overpressure.txt collected_data/viper3d_th_overpressure_s$i.txt
